8db9488077c325b3dae7083cf871f09dacc02d98
Lorenzo Rossi <lorenzo@frollo.net>
2022-09-16 16:08:12 +0200

    Script dockerization

--- scripts/build-bestiary-a4.sh
+++ scripts/build-bestiary-a4.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
-scriptdir="/mnt/c/Users/utente/Documents/GitHub/cairn-ita/scripts"
-sourcedir="/mnt/c/Users/utente/Documents/GitHub/cairn-ita/risorse/mostri"
-tmpdir="/mnt/c/Users/utente/Documents/GitHub/cairn-ita/tmp"
-destdir="/mnt/c/Users/utente/Documents/GitHub/cairn-ita/build"
-mkdir -p $tmpdir/mostri
+
+
+# Parameters validation
+
+if [ -z "$1" ]; then
+	echo "Usage: build-bestiary-a4.sh path/to/cairn/dir"
+	exit 1
+fi
+
+# Directories set up
+
+BASE_DIR=$1
+
+scriptdir="$BASE_DIR/scripts"
+sourcedir="$BASE_DIR/risorse/mostri"
+tmpdir="$BASE_DIR/tmp"
+destdir="$BASE_DIR/build"
+
+if ! [ -x $scriptdir ]; then
+	echo "Script directory $scriptdir does not exist"
+	exit 1
+fi
+
+if ! [ -x $sourcedir ]; then
+	echo "Source directory $sourcedir does not exist"
+	exit 1
+fi
+
+if ! [ -x $tmpdir ]; then
+	mkdir $tmpdir
+fi
+
+if ! [ -x $destdir ]; then
+	mkdir $destdir
+fi
+
+if ! [ -x "$tmpdir/mostri" ]; then
+	mkdir -p $tmpdir/mostri
+fi
+
+# Break if any command exits with error
+set -e
+
 rsync -av $sourcedir/ $tmpdir/mostri/
 sed -i -f $scriptdir/sources/prep.sed $tmpdir/mostri/*.md
 cat $tmpdir/mostri/*.md >> $tmpdir/cairn-bestiary-tmp.md
@@ -11,7 +49,7 @@ cp $scriptdir/sources/a4.tex $tmpdir/cairn-bestiary.tex
 pandoc $tmpdir/cairn-bestiary-tmp.md -f markdown -t latex -o $tmpdir/cairn-bestiary-tmp.tex
 cat $tmpdir/cairn-bestiary-tmp.tex >> $tmpdir/cairn-bestiary.tex
 sed -i '$a \\\end{document}' $tmpdir/cairn-bestiary.tex
-pdflatex -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
-pdflatex -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
+pdflatex -interaction=nonstopmode -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
+pdflatex -interaction=nonstopmode -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
 mv $tmpdir/cairn-bestiary.pdf "$destdir/cairn-bestiario-a4.pdf"
 #rm -rf $tmpdir
--- scripts/sources/prep.sed
+++ scripts/sources/prep.sed
@@ -7,8 +7,8 @@ s/impaired/\\emph{Impaired}/I
 s/blast/\\emph{Blast}/I
 s/Critical Damage/\\textbf{Critical Damage}/I
 s/^- /\\item /g
-11 i \\\begin{samepage}
-11 i \\\begin{itemize}
-11 i \\\setlength\\\itemsep{\-.5em}
-$ a \\\end{itemize}
-$ a \\\end{samepage}
+11 i \\begin{samepage}
+11 i \\begin{itemize}
+11 i \\setlength\\itemsep{\-.5em}
+$ a \\end{itemize}
+$ a \\end{samepage}
